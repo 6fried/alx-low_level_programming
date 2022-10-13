538c9e8b27203872167e6e2eef7e99ffbb95d186
6fried <kellyhantan@gmail.com>
2022-10-13 04:13:05 -0700

    .

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
