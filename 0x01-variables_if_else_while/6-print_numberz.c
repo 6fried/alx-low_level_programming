2c4e32e1f4fbbc337998d106d5144224d09bb436
6fried <kellyhantan@gmail.com>
2022-10-14 10:54:08 -0700

    .

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main-entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
