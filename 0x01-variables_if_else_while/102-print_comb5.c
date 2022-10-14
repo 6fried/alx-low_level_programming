6e31de52d3112d523ba1eaa94667e32aad18b323
6fried <kellyhantan@gmail.com>
2022-10-14 11:52:18 -0700

    .

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,9 +13,9 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = i; j <= '9'; j++)
 		{
-			for (k = i; k <= '9'; k++)
+			for (k = '0'; k <= '9'; k++)
 			{
 				for (l = j + 1; l <= '9'; l++)
 				{
