2d90355d3b564fbf0283fed100da42eff318bf1e
6fried <kellyhantan@gmail.com>
2022-10-14 10:49:03 -0700

    .

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main-entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if(i == 'e' || i == 'q') continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
