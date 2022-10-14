bb574b1d1c3e33fce9a2e675c44fde95dbc57b25
6fried <kellyhantan@gmail.com>
2022-10-14 11:24:08 -0700

    .

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,8 +17,9 @@ int main(void)
 		for (j = i + 1; j <= '9'; j++)
 		{
 			putchar(i);
+			putchar(j);
 
-			if (i != '9')
+			if (!(i == '8' && j == '9'))
 			{
 				putchar(',');
 				putchar(' ');
