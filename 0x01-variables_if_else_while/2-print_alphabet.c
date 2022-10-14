3645eafa4e0a9b180e977e33cab48b61f809e448
6fried <kellyhantan@gmail.com>
2022-10-14 10:41:58 -0700

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+
+/**
+ * main-entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int i = 'a'; i < 'z'; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+}
