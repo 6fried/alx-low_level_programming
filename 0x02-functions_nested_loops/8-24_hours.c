deda4b5198766ef1709377d12da31e0922775f14
6fried <kellyhantan@gmail.com>
2022-10-18 10:14:21 -0700

    .

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 			{
 				for (l = '0'; l <= '9'; l++)
 				{
-					if (!(i == '2' && j >= '4')) 
+					if (!(i == '2' && j >= '4'))
 					{
 						_putchar(i);
 						_putchar(j);
