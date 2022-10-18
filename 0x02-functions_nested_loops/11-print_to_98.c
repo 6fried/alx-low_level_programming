5dfccca940c40fcf4f7f1126e7cc30ba9da36e9d
6fried <kellyhantan@gmail.com>
2022-10-18 16:59:48 -0700

    .

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,44 +8,55 @@
 
 void print_to_98(int n)
 {
-	int i;
+	int i, n_abs;
 
 	if (n < 99)
 	{
 		for (i = n; i < 99; i++)
 		{
-			if (i >= 10)
+			if (i < 0)
 			{
-				_putchar('0' + i / 10);
+				n_abs = -i;
+				_putchar('-');
 			}
-			_putchar('0' + i % 10);
-			if (i != 98)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
+				n_abs = i;
 			}
+			print(n_abs);
 		}
 	}
 	else
 	{
 		for (i = n; i > 97; i--)
 		{
-			if (i >= 100)
-			{
-				_putchar('0' + i / 100);
-				_putchar('0' + (i / 10) % 10);
-			}
-			else
-			{
-				_putchar('0' + i / 10);
-			}
-			_putchar('0' + i % 10);
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			print(i);
 		}
 	}
 	_putchar('\n');
+}
+
+/**
+ * print - prints a number
+ * @i: The number
+ */
+
+
+void print(int i)
+{
+	if (i >= 100)
+	{
+		_putchar('0' + i / 100);
+		_putchar('0' + (i / 10) % 10);
+	}
+	else
+	{
+		_putchar('0' + i / 10);
+	}
+	_putchar('0' + i % 10);
+	if (i != 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
 }
