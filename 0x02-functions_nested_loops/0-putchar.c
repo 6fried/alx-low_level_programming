c340858794a534c379636ed8349c8bee09f746c2
6fried <kellyhantan@gmail.com>
2022-10-18 06:30:00 -0700

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main - entry point
@@ -7,6 +7,14 @@
 
 int main(void)
 {
-	write(1, "_putchar\n", 9);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
