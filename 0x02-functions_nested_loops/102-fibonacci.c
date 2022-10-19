0b2f90f9700dd1b4c0ab594c3a852b5851db9981
6fried <kellyhantan@gmail.com>
2022-10-19 02:20:36 +0100

    .

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - prints fibonqcci of n
+ * @n: the number
+ * Return: f(n)
+ */
+
+int fibonacci(int n)
+{
+	if (n == 0)
+	{
+		printf("%d ", 0);
+		return (0);
+	}
+	else if (n == 1)
+	{
+		printf("%d ", 1);
+		return (1);
+	}
+	else
+	{
+		int r;
+
+		r = fibonacci(n - 2) + fibonacci(n - 1);
+		printf("%d ", r);
+		return (r);
+	}
+}
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	fibonacci(50);
+}
