7e6a6e02cced4013bea1ab94a0e2ea2d0bddb339
6fried <kellyhantan@gmail.com>
2022-10-20 10:26:04 -0700

    .

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-
-
-	/** while (i < 10)
+	/*
+	* while (i < 10)
 	* {
 	*	putchar(i);
-	* }*/
+	* }
+	*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
