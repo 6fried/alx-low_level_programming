d62b39fcd4d9f30fdb8fb3e55b0fa48b91570b5a
6fried <kellyhantan@gmail.com>
2022-10-20 05:02:34 -0700

    .

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,14 @@ void _purchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
 
 #endif
