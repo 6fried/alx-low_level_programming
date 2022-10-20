cc094e0d22f94d79945e43bee6e493355104b1d3
6fried <kellyhantan@gmail.com>
2022-10-20 04:51:52 -0700

    .

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character
+ * Return: 1 if true, 0, else
+ */
+
+int _isdigit(int c);
+{
+	return (c >= '0' && c <= '9');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 
 void _purchar(int c);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
