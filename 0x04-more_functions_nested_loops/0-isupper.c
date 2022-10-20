07972a38f2770b2bcb76307cd27ceb841c46d4b0
6fried <kellyhantan@gmail.com>
2022-10-20 04:35:17 -0700

    .

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ * _isupper - checks for uppercase character.
+ * @c: character
+ * Return: 1 if true, 0, else
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _purchar(int c);
+int _isupper(int c);
+
+#endif
