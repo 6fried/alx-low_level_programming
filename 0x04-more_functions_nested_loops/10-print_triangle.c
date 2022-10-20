8eb0a724c85ced69e337d1ac71298cea26229887
6fried <kellyhantan@gmail.com>
2022-10-20 11:50:13 -0700

    .

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,9 +13,9 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size - i; j++)
+			for (j = i; j < size - 1; j++)
 				_putchar(' ');
-			for (j = size - i; j <= size; j++)
+			for (j = 0; j < i + 1; j++)
 				_putchar('#');
 			_putchar('\n');
 		}
