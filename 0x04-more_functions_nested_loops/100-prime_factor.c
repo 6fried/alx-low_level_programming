7d530dd7fdd4fdca234d2887d1c408d0ec7b1bfb
6fried <kellyhantan@gmail.com>
2022-10-21 11:57:53 -0700

    .

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long i, prime, n = 15;
+
+	for (i = 3; i <= n; i += 2)
+	{
+		while (n % i == 0)
+		{
+			prime = i;
+			n = n / i;
+		}
+	}
+	printf("%lu\n", prime);
+	return (0);
+}
