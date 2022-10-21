0c5d1c34ccd49664756ba85acf11dd9c0a10d62b
6fried <kellyhantan@gmail.com>
2022-10-21 11:59:41 -0700

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,33 +7,33 @@
  */
 void print_number(int n)
 {
-int cont = 1, num = n;
+	int cont = 1, num = n;
 
-if (n < 0)
-_putchar('-');
+	if (n < 0)
+		_putchar('-');
 
-while (num / 10 != 0)
-{
-cont = cont * 10;
-num = num / 10;
-}
+	while (num / 10 != 0)
+	{
+		cont = cont * 10;
+		num = num / 10;
+	}
 
-while ((n / cont != 0) && (cont != 1))
-{
-_putchar(abs((n / cont)) + '0');
+	while ((n / cont != 0) && (cont != 1))
+	{
+		_putchar(abs((n / cont)) + '0');
 
-n = n % cont;
+		n = n % cont;
 
-if (cont > 1)
-cont = cont / 10;
+		if (cont > 1)
+			cont = cont / 10;
 
-while ((abs(n) < cont) &&(cont > 1))
-{
-_putchar('0');
-cont = cont / 10;
-}
-}
+		while ((abs(n) < cont) &&(cont > 1))
+		{
+			_putchar('0');
+			cont = cont / 10;
+		}
+	}
 
-_putchar(abs(n) + '0');
+	_putchar(abs(n) + '0');
 }
 
