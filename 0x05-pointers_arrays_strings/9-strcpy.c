45f5c4d75632a0ca7123dfc85326e2d3814e935d
6fried <kellyhantan@gmail.com>
2022-10-25 00:04:35 +0100

    .

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,16 @@
-char *_strcpy(char *dest, char *src);
+/**
+ * _strcpy - description
+ *
+ * @dest:  argument
+ * @src:  argument
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+}
