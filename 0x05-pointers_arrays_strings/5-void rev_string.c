3047f999df1ce3d659a5bceb0f583ac7739c1983
6fried <kellyhantan@gmail.com>
2022-10-24 23:19:48 +0100

    .

---
+++ 0x05-pointers_arrays_strings/5-void rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ *
+ * @s: the string
+ */
+void rev_string(char *s)
+{
+	char *tmp = s;
+	int i, size;
+
+	size = strlen(tmp);
+	for (i = size - 1; i >= 0; i--)
+	{
+		s[i] = tmp[size - 1];
+	}
+}
