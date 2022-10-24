dd810670e19f6d6e99a5dde7c5b2153289227c18
6fried <kellyhantan@gmail.com>
2022-10-24 23:12:57 +0100

    .

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev- prints a string in reverse
+ *
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i > 0; i++)
+	{
+		_putchar(s[i]);
+	}
+}
