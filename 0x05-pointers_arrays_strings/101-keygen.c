f7d79b37a74e2c6ea03e4a692f3dd27897e757c8
6fried <kellyhantan@gmail.com>
2022-10-25 01:50:11 +0100

    .

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main -func
+ *
+ * Return: return
+ */
+int main(void)
+{
+	char seed[] = "qwertyuioopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+	char password[22];
+	int i, n, passw, cmp;
+
+	srand(time(0));
+
+	for (i = 0; passw < 2772; i++)
+	{
+		n = rand() % 10;
+		password[i] = seed[n];
+		passw += password[i];
+	}
+
+	cmp = 2772 - passw;
+	password[i] = cmp;
+
+	printf("%S\n", password);
+	return (0);
+}
