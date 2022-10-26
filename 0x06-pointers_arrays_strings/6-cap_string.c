2a5162874714dd9b1196edee9dfd7a8a49739614
6fried <kellyhantan@gmail.com>
2022-10-26 23:47:56 +0100

    .

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -35,12 +35,17 @@ char *cap_string(char *str)
 	for (j = 0; str[j]; j++)
 		;
 
-	for (i = 0; i < j; i++)
+	if (('a' <= str[0] && str[0] <= 'z'))
 	{
-		if (in(str[i], delimiters) && 'a' <= str[i + 1] && str[i + 1] <= 'z')
+		str[0] = 'A' - 'a' + str[0]; /*cf. string_toupper*/
+	}
+
+	for (i = 1; i < j; i++)
+	{
+		if (in(str[i], delimiters) && ('a' <= str[i + 1] && str[i + 1] <= 'z'))
 		{
-			str[i + 1] = 'A' - 'a' + str[i + 1]; /*cf. string_toupper*/
 			i++;
+			str[i] = 'A' - 'a' + str[i]; /*cf. string_toupper*/
 		}
 	}
 	return (str);
