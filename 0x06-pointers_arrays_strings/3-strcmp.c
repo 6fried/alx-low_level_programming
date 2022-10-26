07f61c913b9edac84e521a00bbc22b57f8c32f74
6fried <kellyhantan@gmail.com>
2022-10-26 23:09:57 +0100

    .

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,18 +17,23 @@ int _strcmp(char *s1, char *s2)
 	if (i > j)
 		return (15);
 	else if (i == j)
-		return (0);
+	{
+		for (i = 0; s1[i]; i++)
+		{
+			if (s1[i] < s2[i])
+			{
+				return (-15);
+			}
+			else if (s1[i] > s2[i])
+			{
+				return (-15);
+			}
+			else
+			{
+				return (0);
+			}
+		}
+	}
 	else
 		return (-15);
-	/*
-	 *for (i = 0; s1[i]; i++)
-	 * {
-	 *	if (s1[i] != s2[i])
-	 *	{
-	 *		return (0);
-	 *	}
-	 * }
-	 */
-
-	return (1);
 }
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * reverse_array - reverses an array
  *
@@ -18,4 +19,23 @@ void reverse_array(int *a, int n)
 	{
 		a[i] = tmp[n - i - 1];
 	}
+}
+
+int main()
+{
+	int p = 9;
+	int b = 5;
+	int *ptr = &p;
+
+	// printf("%d\n", *ptr);
+
+	ptr = &b;
+	int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+
+	int n[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	char str[13] = "I am a string";
+
+	printf("%d\n", n);
+	printf("%d\n", &n[0]);
 }
