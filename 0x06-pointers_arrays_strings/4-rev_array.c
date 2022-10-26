37b6f6dd39edc64b3cd0c3b12acec1a647f9c00b
6fried <kellyhantan@gmail.com>
2022-10-26 23:34:51 +0100

    .

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,7 +8,7 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int tmp[n];
+	int tmp[100];
 
 	for (i = 0; i < n; i++)
 	{
@@ -19,23 +19,4 @@ void reverse_array(int *a, int n)
 	{
 		a[i] = tmp[n - i - 1];
 	}
-}
-
-int main()
-{
-	int p = 9;
-	int b = 5;
-	int *ptr = &p;
-
-	// printf("%d\n", *ptr);
-
-	ptr = &b;
-	int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
-
-	int n[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-	char str[13] = "I am a string";
-
-	printf("%d\n", n);
-	printf("%d\n", &n[0]);
 }
