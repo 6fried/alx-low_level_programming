31fd80551800749d05cdd75223f9b4c6cbe72428
6fried <kellyhantan@gmail.com>
2022-10-26 23:20:42 +0100

    .

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,25 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i, j, k, min;
 
 	for (i = 0; s1[i]; i++)
 		;
 	for (j = 0; s2[j]; j++)
 		;
 
-	for (i = 0; s1[i]; i++)
+	if (i > j)
 	{
-		if (s1[i] < s2[i])
+		min = j;
+	}
+	else
+	{
+		min - i;
+	}
+
+	for (k = 0; k < min; k++)
+	{
+		if (s1[k] < s2[k])
 		{
 			return (-15);
 		}
@@ -24,6 +33,14 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (15);
 		}
+	}
+	if (i > j)
+	{
+		return (15);
+	}
+	else
+	{
+		return (-15);
 	}
 	return (0);
 }
