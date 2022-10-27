27044d9a292501f2580b394437984a099d4f73dd
6fried <kellyhantan@gmail.com>
2022-10-27 21:49:24 +0100

    .

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,91 +1,54 @@
+#include "holberton.h"
+#include <stdio.h>
 /**
- * print_number -  adds two numbers.
- *
- * @n1: param
- * @2: param
- * @r: param
- * @size_r: param
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, re, n;
-	int size_n1, size_n2;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	for (i = 0; n1[i]; i++)
-		;
-	size_n1 = i;
-	for (i = 0; n2[i]; i++)
-		;
-	size_n2 = i;
-
-	if (size_r < size_n1 || size_r < size_n2)
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-
-	r[size_r - 1] = '\0';
-
-	k = size_r;
-	re = 0;
-
-	for (i = size_n1 - 1, j = size_n2 - 1; i >= 0, j >= 0; i--, j--, k--)
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		if (k < 0)
-			return (0);
-		n = n1[i] + n2[j] + re - '0' * 2;
-		r[k] = '0' + n % 10;
-		re = (n - n % 10) / 10;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+			*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
-	return (r);
-}
-
-int main(void)
-{
-	char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
-	char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
-	char r[100];
-	char r2[10];
-	char r3[11];
-	char *res;
-
-	res = infinite_add(n, m, r, 100);
-	if (res == 0)
-	{
-		printf("Error\n");
-	}
+	if (*(r) == '0')
+		return (r + 1);
 	else
-	{
-		printf("%s + %s = %s\n", n, m, res);
-	}
-	n = "1234567890";
-	m = "1";
-	res = infinite_add(n, m, r2, 10);
-	if (res == 0)
-	{
-		printf("Error\n");
-	}
-	else
-	{
-		printf("%s + %s = %s\n", n, m, res);
-	}
-	n = "999999999";
-	m = "1";
-	res = infinite_add(n, m, r2, 10);
-	if (res == 0)
-	{
-		printf("Error\n");
-	}
-	else
-	{
-		printf("%s + %s = %s\n", n, m, res);
-	}
-	res = infinite_add(n, m, r3, 11);
-	if (res == 0)
-	{
-		printf("Error\n");
-	}
-	else
-	{
-		printf("%s + %s = %s\n", n, m, res);
-	}
-	return (0);
+		return (r);
 }
