277eab5e45ed430bd0d31ffbf2eac429fde36a01
6fried <kellyhantan@gmail.com>
2022-10-27 01:00:03 +0100

    .

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,8 +8,37 @@
  */
 void print_number(int n)
 {
-	int i;
+	int i, j;
+	char nbr[100];
 
-	i = n % 10 + '0';
-	write(1, &i, 1);
+	i = 0;
+	if (n >= 0)
+	{
+		j = n;
+		nbr[0] = '+';
+	}
+	else
+	{
+		j = -n;
+		nbr[0] = '-';
+	}
+
+	do
+	{
+		i++;
+	} while (j /= 10);
+
+	nbr[i + 1] = '\0';
+	for (i = i; i > 0; i--)
+	{
+		nbr[i] = '0' + j % 10;
+		j = j - j % 10;
+		j = j / 10;
+	}
+
+	for (i = 0; nbr[i]; i++)
+	{
+		if (nbr[i] != '+')
+			_putchar(nbr[i]);
+	}
 }
