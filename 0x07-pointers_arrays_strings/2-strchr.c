38546ff838ffa011b37778aa4f73aab08f10126f
6fried <kellyhantan@gmail.com>
2022-10-31 10:26:28 -0700

    .

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -21,3 +21,17 @@ char *_strchr(char *s, char c)
 
 	return (NULL);
 }
+
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
+}
