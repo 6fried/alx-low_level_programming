cd08c35126de57670e9c845cfbbfc8c515c7bf60
6fried <kellyhantan@gmail.com>
2022-10-31 10:08:46 -0700

    .

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - function
+ * @s: param
+ * @b: param
+ * @n: param
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i > n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
