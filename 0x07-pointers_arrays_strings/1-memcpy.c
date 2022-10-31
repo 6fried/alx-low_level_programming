37c7c8c9e9ff53c905bb54eec5cab1d5c6a12ac4
6fried <kellyhantan@gmail.com>
2022-10-31 10:21:34 -0700

    .

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function
+ * @dest: param
+ * @src: param
+ * @n: param
+ * Return: pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (s);
+}
