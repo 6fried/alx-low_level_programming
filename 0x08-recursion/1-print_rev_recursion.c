201a42219ae74fc749f08ee40c29120dba55c810
6fried <kellyhantan@gmail.com>
2022-11-02 03:17:06 -0700

    .

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *  print_n - recursion
+ * @s: param
+ * @n: param
+ */
+void print_n(char *s, int n)
+{
+	if (s[n] != '\0')
+	{
+		write(1, &s[n], 1);
+		print_n(s, n + 1);
+	}
+}
+/**
+ * size - recursion
+ * @s: param
+ * @n: param
+ * Return: int
+ */
+int size(char s, int n)
+{
+	if (s[n] == '\0')
+		return n;
+	return size(s, n+1);
+}
+
+/**
+ *  _print_rev_recursion - recursion
+ * @s: param
+ */
+void _print_rev_recursion(char *s)
+{
+	int i;
+
+	i = size(s, 0);
+	print_n(s, i);
+	write(1, "\n", 1);
+}
+
