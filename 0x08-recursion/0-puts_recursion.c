bc7a7c2011ae30d9099803f9d91ea6f9a4c9ebc5
6fried <kellyhantan@gmail.com>
2022-11-02 03:04:30 -0700

    .

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,24 @@
 #include "main.h"
+
+/**
+ *  print_n - recursion
+ * @s: param
+ * @n: param
+ */
+void print_n (char *s, int n)
+{
+	if (s[n])
+	{
+		write(1, &s[n], 1);
+		print_n(s, n + 1);
+	}
+}
+
 /**
  *  _puts_recursion - recursion
- * @s: pram
+ * @s: param
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] != '\0')
-	{
-		int i;
-
-		write(1, &s[0], 1);
-
-		for (i = 0; s[i]; i++)
-			s[i] = s[i + 1];
-		_puts_recursion(s);
-	}
+	print_n(0);
 }
