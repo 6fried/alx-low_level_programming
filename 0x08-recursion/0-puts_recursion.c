9a364ab5e15eb9907d826877f65da961cce9c516
6fried <kellyhantan@gmail.com>
2022-11-02 03:09:49 -0700

    .

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,9 +5,9 @@
  * @s: param
  * @n: param
  */
-void print_n (char *s, int n)
+void print_n(char *s, int n)
 {
-	if (s[n])
+	if (s[n] != '\0')
 	{
 		write(1, &s[n], 1);
 		print_n(s, n + 1);
@@ -21,4 +21,6 @@ void print_n (char *s, int n)
 void _puts_recursion(char *s)
 {
 	print_n(s, 0);
+	write (1, "\n", 1);
 }
+
