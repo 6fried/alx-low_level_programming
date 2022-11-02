b4cbd86dd29ea247496a017bfdd5356c5ce0748d
6fried <kellyhantan@gmail.com>
2022-11-02 03:32:45 -0700

    .

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+/**
+ * factorial - recursion
+ * @n: param
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if  (n == 0)
+		return (1);
+	else (return (n * factorial(n - 1));
+}
