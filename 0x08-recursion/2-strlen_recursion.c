2dd4c3845390acc60480406f4e7d452d50985dee
6fried <kellyhantan@gmail.com>
2022-11-02 03:25:36 -0700

    .

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * size - recursion
+ * @s: param
+ * @n: param
+ * Return: int
+ */
+int size(char *s, int n)
+{
+	if (s[n] == '\0')
+		return (n);
+	return (size(s, n + 1));
+}
+
+/**
+ *  _strlen_recursion - recursion
+ * @s: param
+ */
+int _strlen_recursion(char *s)
+{
+	return (size(s, 0);
+}
