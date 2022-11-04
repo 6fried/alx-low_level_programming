797203eacaa9ad7a509d5dda55da84cba45c9adb
6fried <kellyhantan@gmail.com>
2022-11-04 05:50:43 -0700

    .

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,8 @@
  * @argv: param
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
-	if (argc == 0)
-	{}
 	printf("%s\n", argv[0]);
 	return (0);
 }
