776b64540ee17d88c7dbe67f1b540dd20d99e4c3
6fried <kellyhantan@gmail.com>
2022-11-04 06:36:37 -0700

    .

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,33 +10,35 @@ int main(int argc, char **argv)
 {
 	int i, a, b;
 
-	if (argc < 2)
+	if (argc > 1)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 0; argv[1][i]; i++)
-	{
-		if (argv[1][i] > '9' || argv[1][i] < '0')
+		for (i = 0; argv[1][i]; i++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[1][i] > '9' || argv[1][i] < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		a = atoi(argv[1]);
 	}
+	else
+		a = 0;
 
-	for (i = 0; argv[2][i]; i++)
+	if (argc > 2)
 	{
-		if (argv[2][i] > '9' || argv[2][i] < '0')
+		for (i = 0; argv[2][i]; i++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[2][i] > '9' || argv[2][i] < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-
+		b = atoi(argv[2]);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
+	else
+		b = 0;
 
 	printf("%d\n", a + b);
 	return (0);
