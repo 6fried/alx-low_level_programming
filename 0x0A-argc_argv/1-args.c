ca3987bf21b9d5399f0796427a25a21f79a6eba1
6fried <kellyhantan@gmail.com>
2022-11-04 05:54:14 -0700

    .

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: param
+ * @argv: param
+ * Return: 0
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
