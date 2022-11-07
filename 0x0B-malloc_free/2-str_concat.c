e25802a62b15a986218c22cb2a27ffcfc8c892d6
6fried <kellyhantan@gmail.com>
2022-11-07 14:11:37 -0800

    .

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * str_concat - func
+ * @s1: param
+ * @s2: param
+ * Return: char*
+ */
+
+char *str_concat(char *s1, char *s2);
+{
+	int i, j, k;
+	char *out;
+
+	if (s1 == NULL)
+		j = 0;
+	else
+		for (j = 0; s1[j]; j++)
+			;
+
+	if (s2 == NULL)
+		k = 0;
+	else
+		for (k = 0; s2[k]; k++)
+			;
+	k++;
+
+	out = malloc((j + k) * sizeof(char));
+	if (out == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		out[i] = s1[i];
+
+	for (j = 0; s2[j]; j++)
+		out[i+j+1] = s2[j];
+
+	return (out);
+}
