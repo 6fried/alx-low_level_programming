0f0ffe610e6b240940ef01f6649ab78a63dedb50
6fried <kellyhantan@gmail.com>
2022-11-07 15:47:25 -0800

    .

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* free_grid - func
+* @grid: param
+* @heeight: param
+*/
+void free_grid(int **grid, int height);
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
