f58412a0ef5080a040aea1f0011ba77b87e92f01
6fried <kellyhantan@gmail.com>
2022-11-07 13:45:35 -0800

    .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *out = malloc(size * sizeof(char));
 
+	if (size == 0)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		out[i] = c;
 
