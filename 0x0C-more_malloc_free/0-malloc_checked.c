487b369d1fe1857bd1e0a282d175ef952a49c042
6fried <kellyhantan@gmail.com>
2022-11-10 13:39:32 -0800

    .

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - func
+ * @b: param
+ * Return: void *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *out = malloc(b);
+
+	if (out == NULL)
+		exit(98);
+
+	return out;
+}
